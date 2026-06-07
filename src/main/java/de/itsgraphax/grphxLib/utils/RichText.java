02981540980a4a9ed4c099058682e3033e1b2411
ItsGraphax <itsgraphax@proton.me>
2026-06-07 21:59:33 +0200

    RichText param functions are now deprecated as the non-param functions now also support params

--- src/main/java/de/itsgraphax/grphxLib/utils/RichText.java
+++ src/main/java/de/itsgraphax/grphxLib/utils/RichText.java
@@ -19,26 +19,25 @@ public class RichText {
     }
 
     /**
-     * Converts MiniMessage format to Components
-     * @param richMessage A String in MiniMessage Format
-     * @return The parsed String
-     */
-    public @NotNull Component parse(@NotNull String richMessage) {
-        return miniMessage.deserialize(richMessage);
-    }
-
-    /**
      * Parses MiniMessage text with params
      * @param params Arguments in the order of (key, value), for example: `paramsParse("key", "POINTS", points, "NAME", name)`
      * @return The parsed string with all instances of "{{key}}" replaced with the value
      */
-    public @NotNull Component paramsParse(@NotNull String richMessage, String... params) {
+    public @NotNull Component parse(@NotNull String richMessage, String... params) {
         for (int i = 0; i < params.length; i += 2) {
             String key = params[i];
             String val = params[i + 1];
             richMessage = richMessage.replace(String.format("{{%s}}", key), val);
         }
-        return parse(richMessage);
+        return miniMessage.deserialize(richMessage);
+    }
+
+    /**
+     * @deprecated This is now an alias for parse, with the same function
+     */
+    @Deprecated(since="1.1.0", forRemoval = true)
+    public @NotNull Component paramsParse(@NotNull String richMessage, String... params) {
+        return parse(richMessage, params);
     }
 
     public @NotNull String serialize(@NotNull Component component) {
@@ -56,24 +55,23 @@ public class RichText {
         }
 
         /**
-         * Parses MiniMessage text from config
-         * @param subkey A key in config that gets automatically prefixed by "strings."
-         * @return The parsed String
-         */
-        public @NotNull Component fromConfig(@NotNull String subkey) {
-            return parse(config.getString("strings." + subkey, String.format("!%s", subkey)));
-        }
-
-        /**
          * Parses MiniMessage text from config with params
          * @param subkey A key in config that gets automatically prefixed by "strings."
          * @param params Arguments in the order of (key, value), for example: `paramsFromConfig("key", "POINTS", points, "NAME", name)`
          * @return The parsed string with all instances of "{{key}}" replaced with the value
          */
-        public @NotNull Component paramsFromConfig(@NotNull String subkey, String... params) {
+        public @NotNull Component fromConfig(@NotNull String subkey, String... params) {
             if (params.length % 2 != 0) throw new IllegalArgumentException("The number of params needs to consist of a key, then value; for example ('POINTS', points, 'NAME', name)");
             String richText = config.getString("strings." + subkey, String.format("!%s%s", subkey, Arrays.toString(params)));
-            return paramsParse(richText, params);
+            return parse(richText, params);
+        }
+
+        /**
+         * @deprecated This is now an alias for RichConfigText#fromConfig
+         */
+        @Deprecated(since="1.1.0", forRemoval = true)
+        public @NotNull Component paramsFromConfig(@NotNull String subkey, String... params) {
+            return fromConfig(subkey, params);
         }
     }
 }
