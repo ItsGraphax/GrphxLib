2b774d458164afedd4196dc4d92c81c09553167a
ItsGraphax <126398781+ItsGraphax@users.noreply.github.com>
2026-08-07 23:09:57 +0200

    Turns out debugging wasnt neccessary <3

--- src/main/java/de/itsgraphax/grphxLib/citems/CitemListener.java
+++ src/main/java/de/itsgraphax/grphxLib/citems/CitemListener.java
@@ -17,15 +17,15 @@ public class CitemListener implements Listener {
     @EventHandler
     void onInteract(PlayerInteractEvent event) {
         if (event.useInteractedBlock() == Event.Result.ALLOW) return;
-        System.out.println("1");
+
         if (event.getHand() != EquipmentSlot.HAND) return;
-        System.out.println("2");
+
         ItemStack item = event.getItem();
         if (item == null) return;
-        System.out.println("3");
+
         Citem citem = manager.fromItem(event.getItem());
         if (citem == null) return;
-        System.out.println("4");
+
         citem.onInteract(event);
     }
 }
